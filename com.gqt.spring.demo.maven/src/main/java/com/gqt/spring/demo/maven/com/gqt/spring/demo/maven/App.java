0d019aa66fb7ef228cb4a1ad3b07f2258a08ef18
ravitejagangula <114054501+ravitejacodes@users.noreply.github.com>
2024-06-20 12:54:34 +0530

    Add files via upload

---
+++ com.gqt.spring.demo.maven/src/main/java/com/gqt/spring/demo/maven/com/gqt/spring/demo/maven/App.java
@@ -0,0 +1,13 @@
+package com.gqt.spring.demo.maven.com.gqt.spring.demo.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
